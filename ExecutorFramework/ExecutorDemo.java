98f9c55b0ba31f9e2dae13e34d105792fbebdef0
YogeshPriye <yogeshpriye8797@gmail.com>
2023-02-25 16:45:34 +0530

    commit

---
+++ ExecutorFramework/ExecutorDemo.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorDemo {
+
+	public static void main(String[] args) {
+		CheckProcessorTask cps[] = { new CheckProcessorTask("ATM"), new CheckProcessorTask("Bank"),
+				new CheckProcessorTask("Mobile"), new CheckProcessorTask("web") };
+		
+		ExecutorService service = Executors.newFixedThreadPool(2);
+		
+		for (CheckProcessorTask checkProcessorTask : cps) {
+			service.submit(checkProcessorTask);
+		}
+		service.shutdown();
+	}
+
+}
